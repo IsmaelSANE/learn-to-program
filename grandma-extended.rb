334cb19db6cbe4072dacd6f5d8241a04b7fbf655
Ismael Sane <six.echo.sa@gmail.com>
2021-04-23 14:23:29 +0200

    Refactor the code

--- grandma-extended.rb
+++ grandma-extended.rb
@@ -4,18 +4,17 @@ ask_grandma = gets.chomp
 leave_response = 1
 
 while leave_response < 3
-
-  if ask_grandma == 'BYE'
-    leave_response += 1
-    puts "I CAN'T HEAR YOU"
-
-  elsif ask_grandma == ask_grandma.upcase
-    old_date = 1930 + rand(21)
-    puts "NO, NOT SINCE #{old_date}!"
-    leave_response = 1
-  else
-    puts grandma_response
-    leave_response = 1
+  case ask_grandma
+   when 'BYE'
+     leave_response += 1
+     puts "I CAN'T HEAR YOU"
+   when ask_grandma.upcase
+     old_date = 1930 + rand(21)
+     puts "NO, NOT SINCE #{old_date}!"
+     leave_response = 1
+   else
+     puts grandma_response
+     leave_response = 1
   end
   ask_grandma = gets.chomp
 end
